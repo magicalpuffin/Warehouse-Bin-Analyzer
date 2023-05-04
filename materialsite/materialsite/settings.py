44a2c16b9747172bf9a4e443e0f152d472bb0ab1
Michael Li <36088648+magicalpuffin@users.noreply.github.com>
2023-05-03 21:54:20 -0700

    remove debug

--- materialsite/materialsite/settings.py
+++ materialsite/materialsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', 'localhost')]
